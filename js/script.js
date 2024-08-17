71ceac97bfb5d96c78d3752a38beea037b233788
doaaapdelhai <doaaapdelhai@gmail.com>
2024-08-17 15:55:17 +0300

    'Completed navbar and slider sections'

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    sideBar.classList.toggle('active');
+}
+document.querySelector('#close-side-bar').onclick = () =>{
+    sideBar.classList.remove('active');
+}
+// ----------
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    sideBar.classList.remove('active');
+    searchForm.classList.remove('active');
+};
+
+// swiper
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabcursor:true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
